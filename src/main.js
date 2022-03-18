805c56946eb7dbe4fcc716659c09ad609157f98c
Andyysv <andreasanchezvel@hotmail.com>
2022-03-18 00:07:58 -0600

    intento de cartas

--- src/main.js
+++ src/main.js
@@ -13,8 +13,24 @@ console.log(pokemon3);
 
 
 
-//Mezcla de cartas (array)
-function mix () { 
+//Mezcla de cartas (fisher yates)
+const mix = pokemon3.sort(() => { //ordena los elementos de un arreglo (array) localmente y devuelve el arreglo ordenado. La ordenación no es necesariamente estable. El modo de ordenación por defecto responde a la posición del valor del string de acuerdo a su valor Unicode
+    return Math.random() - 0.5; //Math. random() retorna un punto flotante, un número pseudo-aleatorio dentro del rango [0, 1). Esto es, desde el 0 (Incluido) hasta el 1 pero sin incluirlo (excluido), el cual se puede escalar hasta el rango deseado. random() devuelve un número aleatorio entre 0.0 y 1.0, excluido este último valor
+});
+/*function fisherYatesShuffle(pokemon3){
+    for(let i =pokemon3.length-1 ; i>0 ;i--){
+        let j = Math.floor( Math.random() * (i + 1) ); //random index
+        [pokemon3[i],pokemon3[j]]=[pokemon3[j],pokemon3[i]]; // swap
+    }
+}
+let tmpArray = [pokemon3];
+fisherYatesShuffle(tmpArray);
+console.log(tmpArray);*/
+//var tmpArray = [1, 3, 5];
+//fisherYatesShuffle(tmpArray);
+
+
+/*function mix () { 
     for (let i= pokemon3.length -1; i>0; i--){
     let r = Math.floor(Math.random() * (i + 1));
     pokemon3 [i], pokemon3 [r] = [pokemon3[r], pokemon3[i]];
@@ -22,28 +38,31 @@ function mix () {
 console.log(pokemon3)
 }
 
-mix ([pokemon3]);
+mix ([pokemon3]);*/
 //Random
 /*const randomCard = () =>{
     const cardData = getData ();
 
 }*/
+const section = document.querySelector("section");
+//Función de las cartas
+const pokemonCard = () => {
+    const cardData = mix();
+    //HTML
+cardData.forEach((item) => {
+    const card = document.createElement("div");
+    const face = document.createElement ("img");
+    const back = document.createElement("div");
+    card.classList = "card";
+    face.classList = "face";
+    back.classList = "back";
+    //info de las cartas
+    face.src=item.image;
+    //Cards section
+    section.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back);
 
-//Mostrar las cartas
-/*const pokemonCard = (pokemon) => {
-const card = document.createElement("div");
-pokemon.forEach (pokemon => { 
-const face = document.createElement ("img");
-const back = document.createElement("div");
-card.classList = "card";
-face.classList = "face";
-back.classList = "back";
-face.src = pokemon.image;
-const name = document.createElement("caption");
-name.textContent= pokemon.id;
-pokemonCards.appendChild(card);
-card.appendChild(face);
-card.appendChild(back);
-})
-return pokemonCards;
-}*/
+});
+}; 
+pokemonCard();
