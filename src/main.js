7cdfd7b404bf86979f6f53a462ddb95c35aed79c
Andyysv <andreasanchezvel@hotmail.com>
2022-03-29 14:15:38 -0600

    tarjetas giratorias sin acomodar pero ya estructuradas

--- src/main.js
+++ src/main.js
@@ -15,67 +15,36 @@ const mix = pokemon3.sort(() => {
   //ordena los elementos de un arreglo (array) localmente y devuelve el arreglo ordenado. La ordenación no es necesariamente estable. El modo de ordenación por defecto responde a la posición del valor del string de acuerdo a su valor Unicode
   return Math.random() - 0.5; //Math. random() retorna un punto flotante, un número pseudo-aleatorio dentro del rango [0, 1). Esto es, desde el 0 (Incluido) hasta el 1 pero sin incluirlo (excluido), el cual se puede escalar hasta el rango deseado. random() devuelve un número aleatorio entre 0.0 y 1.0, excluido este último valor
 });
-/*function fisherYatesShuffle(pokemon3){
-    for(let i =pokemon3.length-1 ; i>0 ;i--){
-        let j = Math.floor( Math.random() * (i + 1) ); //random index
-        [pokemon3[i],pokemon3[j]]=[pokemon3[j],pokemon3[i]]; // swap
-    }
-}
-let tmpArray = [pokemon3];
-fisherYatesShuffle(tmpArray);
-console.log(tmpArray);*/
-//var tmpArray = [1, 3, 5];
-//fisherYatesShuffle(tmpArray);
 
-/*function mix () { 
-    for (let i= pokemon3.length -1; i>0; i--){
-    let r = Math.floor(Math.random() * (i + 1));
-    pokemon3 [i], pokemon3 [r] = [pokemon3[r], pokemon3[i]];
-}
-console.log(pokemon3)
-}
-
-mix ([pokemon3]);*/
-//Random
-/*const randomCard = () =>{
-    const cardData = getData ();
-
-}*/
-/*const section = document.querySelector("section");
-//Función de las cartas
-const pokemonCard = () => {
-    const cardData = mix();
-    //HTML
-cardData.forEach((item) => {
-    const card = document.createElement("div");
-    const face = document.createElement ("img");
-    const back = document.createElement("div");
-    card.classList = "card";
-    face.classList = "face";
-    back.classList = "back";
-    //info de las cartas
-    face.src=item.image;
-    //Cards section
-    section.appendChild(card);
-    card.appendChild(face);
-    card.appendChild(back);
-
-});
-}; 
-inner.HTML= pokemonCard();*/
+//mostrar data en pantalla + carta de frente y atrás por medio de interpolación de variables
 export let showCards = "";
 for (let index = 0; index < 18; index++) {
   const print = document.getElementById("marginNeon");
-  showCards += ` <section class="cardPrueba hidden">
-<img id="image" src = "${mix[index].image}"> 
+  showCards += `
+  <section class="carta-box">
+  <section class="carta">    
+    <section class="cara">
+      <img src="${"../MM/back.png"}" width="200" height="250px">
+    </section>
+    <section class="cara detras">
+      <img src="${mix[index].image}" width="200" height="250px"> <h3> ${mix[index].id} </h3> 
+    </section>    
+  </section>
+</section>
+  `;
+
+  /*<section class="cardPrueba hidden">
+<section class="front" id="front"> <img id="image" src = "" <h3> ${mix[index].id} </h3> 
+</div>
+<div class= "back"> 
 <img id=${index} src = "${"../MM/back.png"}" class=imagenBack>
 </div>
-</section> `;
+</section>*/
 
   print.innerHTML = showCards;
 }
-
-const showPokemon = document.getElementById("hidden");
+//funcion para segun ocultar cartas
+const showPokemon = document.getElementById("front");
 showPokemon.addEventListener("click", () => {
   document.getElementById("cardPrueba").style.display = "none";
   document.getElementById("cardPrueba").style.display = "block";
